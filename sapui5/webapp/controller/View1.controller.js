dea9785cfd414353a91573d6c74fcc289a0fd31a
Florin Floricel <florin.floricel@bearingpoint.com>
2021-11-15 07:41:23 +0000

    first commit

---
+++ sapui5/webapp/controller/View1.controller.js
@@ -0,0 +1,35 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+],
+	/**
+	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 */
+	function (Controller, MessageToast, JSONModel, ResourceModel) {
+		"use strict";
+
+		return Controller.extend("sapui5.controller.View1", {
+            onInit: function () {
+                var oData = {
+                    recipient: {
+                        name: 'World'
+                    }
+                };
+                var oModel = new JSONModel(oData);
+                this.getView().setModel(oModel);
+
+                var i18nModel = new ResourceModel({
+                    bundleName: "sapui5.i18n.i18n"
+                });
+                this.getView().setModel(i18nModel, 'i18n')
+            },
+            onButtonPress: function () {
+                var oBundle = this.getView().getModel('i18n').getResourceBundle();
+                var sRecipient = this.getView().getModel().getProperty('/recipient/name');
+                var sMessage = oBundle.getText('helloMessage', [sRecipient]);
+                MessageToast.show(sMessage);
+            }
+		});
+	});
